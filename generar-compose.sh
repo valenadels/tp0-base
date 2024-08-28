51b6ed1f11ecbc0912614e8012a6291ecf8eba1c
valenadels <v.adelsflugel@despegar.com>
2024-08-28 14:59:55 -0300

    sh script

--- generar-compose.sh
+++ generar-compose.sh
@@ -1,25 +1,66 @@
 #!/bin/bash
-outputFile=$1
+
+function validate_input() {
+    if [ -z $output_file ] || [ -z $clients ]; then
+        echo "Error: Please provide the output file name and the number of clients"
+        exit 1
+    fi
+
+    if ! [[ "$clients" =~ ^[0-9]+$ ]]; then
+        echo "Error: clients should be an integer"
+        exit 1
+    fi
+
+    if [[ $output_file != *yaml ]]; then
+        echo "Error: Output file must end with .yaml"
+        exit 1
+    fi
+}
+
+function write_server_config() {
+    echo "name: tp0" > $output_file
+    echo "services:" >> $output_file
+    echo "  server:" >> $output_file
+    echo "    container_name: server" >> $output_file
+    echo "    image: server:latest" >> $output_file
+    echo "    entrypoint: python3 /main.py" >> $output_file
+    echo "    environment:" >> $output_file
+    echo "      - PYTHONUNBUFFERED=1" >> $output_file
+    echo "      - LOGGING_LEVEL=DEBUG" >> $output_file
+    echo "    networks:" >> $output_file
+    echo "      - testing_net" >> $output_file
+}
+
+function write_client_config() {
+    for ((i=1; i<$clients+1; i++))
+    do
+        echo "  client$i:" >> $output_file
+        echo "    container_name: client$i" >> $output_file
+        echo "    image: client:latest" >> $output_file
+        echo "    entrypoint: /client" >> $output_file
+        echo "    environment:" >> $output_file
+        echo "      - CLI_ID=$i" >> $output_file
+        echo "      - CLI_LOG_LEVEL=DEBUG" >> $output_file
+        echo "    networks: [testing_net]" >> $output_file
+        echo "    depends_on: [server]" >> $output_file
+    done
+}
+
+function write_network_config() {
+    echo "networks:" >> $output_file
+    echo "  testing_net:" >> $output_file
+    echo "    ipam:" >> $output_file
+    echo "      driver: default" >> $output_file
+    echo "      config:" >> $output_file
+    echo "        - subnet: 172.25.125.0/24" >> $output_file
+}
+
+output_file=$1
 clients=$2
 
-if [ -z $outputFile ] || [ -z $clients ]; then
-    echo "Error: Please provide the output file name and the number of clients"
-    exit 1
-fi
-
-if ! [[ "$clients" =~ ^[0-9]+$ ]]; then
-    echo "Error: clients should be an integer"
-    exit 1
-fi
-
-echo "Output file: $1"
-echo "N° clients: $2"
-
-python3 compose-generator/docker-compose-generator.py $1 $2
-
-if [ $? -ne 0 ]; then
-    echo "Error: Python script execution failed"
-    exit 1
-fi
+validate_input
+write_server_config
+write_client_config
+write_network_config
 
 echo "Docker-compose file $1 generated successfully"
