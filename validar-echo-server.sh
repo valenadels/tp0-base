f61cb86218843ef847ef1625cf6b3cbba7b18ce0
valenadels <v.adelsflugel@despegar.com>
2024-09-02 20:56:05 -0300

    fix sh

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,7 +2,6 @@
 
 message="hello world"
 server_port=$(grep SERVER_PORT server/config.ini | cut -d ' ' -f 3)
-echo "server_port: $server_port"
 
 response=$(docker run --rm --network tp0_testing_net busybox:latest /bin/sh -c "echo $message | nc server $server_port")
 
