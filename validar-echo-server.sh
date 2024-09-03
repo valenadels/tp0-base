f96e10d7f8c163611278e4c17e75191d4c279fc5
valenadels <v.adelsflugel@despegar.com>
2024-09-02 21:24:22 -0300

    tests

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -4,7 +4,7 @@ message="hello world"
 SERVER_PORT=$(grep SERVER_PORT server/config.ini | cut -d ' ' -f 3)
 SERVER_IP=$(grep SERVER_IP server/config.ini | cut -d ' ' -f 3)
 
-response=$(docker run --rm --network tp0_testing_net busybox:latest sh -c "echo $message | nc $SERVER_IP $SERVER_PORT")
+response=$(docker run --rm --network tp0_testing_net busybox:latest sh -c "echo '$message' | nc $SERVER_IP $SERVER_PORT")
 
 if [ "$response" = "$message" ]; then
     echo "action: test_echo_server | result: success"
