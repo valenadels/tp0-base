08f5b2ef55206c76dc57ab395446e5a5bc24f67c
valenadels <v.adelsflugel@despegar.com>
2024-09-02 21:18:11 -0300

    fix sh validar

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 message="hello world"
-server_port=$(grep SERVER_PORT server/config.ini | cut -d ' ' -f 3)
+SERVER_PORT=$(grep SERVER_PORT server/config.ini | cut -d ' ' -f 3)
+SERVER_IP=$(grep SERVER_IP server/config.ini | cut -d ' ' -f 3)
 
-response=$(docker run --rm --network tp0_testing_net busybox:latest /bin/sh -c "echo $message | nc server $server_port")
+response=$(docker run --rm --network tp0_testing_net busybox:latest sh -c "echo $message | nc $SERVER_IP $SERVER_PORT")
 
 if [ "$response" = "$message" ]; then
     echo "action: test_echo_server | result: success"
