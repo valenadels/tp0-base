5f4a87cd1cc33484ed79f6d6c70a3e69c9b3bb5a
valenadels <v.adelsflugel@despegar.com>
2024-09-02 21:08:14 -0300

    eq change

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -5,7 +5,7 @@ server_port=$(grep SERVER_PORT server/config.ini | cut -d ' ' -f 3)
 
 response=$(docker run --rm --network tp0_testing_net busybox:latest /bin/sh -c "echo $message | nc server $server_port")
 
-if [ "$response" == "$message" ]; then
+if [ "$response" = "$message" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
